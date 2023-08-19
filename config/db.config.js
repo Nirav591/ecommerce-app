de207480e3daaa30683209950182cdad76832426
Nirav591 <nirav.v.lathiya@gmail.com>
2023-08-19 14:24:33 +0530

    addded database name

--- config/db.config.js
+++ config/db.config.js
@@ -3,6 +3,6 @@ const mysql = require('mysql');
 exports.connection = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'rC2nl8SAo3nHb9YU',
   database: 'ecommerce',
 });
