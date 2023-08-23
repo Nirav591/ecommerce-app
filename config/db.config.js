c1cf967549792810c9aa947a1cbc399d50ab4cd8
Nirav591 <nirav.v.lathiya@gmail.com>
2023-08-23 16:34:31 +0530

    change in the database

--- config/db.config.js
+++ config/db.config.js
@@ -1,8 +1,14 @@
 const mysql = require('mysql');
 
+// exports.connection = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'ecommerce',
+// });
 exports.connection = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: '',
+  user: 'admin',
+  password: 't7x?}>rbmCa~we+',
   database: 'ecommerce',
 });
